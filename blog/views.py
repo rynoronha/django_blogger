483a0353a5b50a1ceb9de9e394783706ddca81a3
Ryan Noronha <ryannoronha@Ryans-MacBook-Pro.local>
2020-01-06 14:55:08 -0300

    templates and styling

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'RyanN',
+        'title': 'Blog Post 1',
+        'content': 'First Post',
+        'date_posted': 'Dec 12, 2019'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second Post',
+        'date_posted': 'Dec 25, 2019'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
